f30f67a42c98d771376f869f27dbf6e32e802713
Jackson Collins-Ikpe <jacksoncikpe@gmail.com>
2022-06-02 14:00:21 +0100

    Added elevated scrolling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,8 +12,23 @@ import {
   Tab,
   useTheme,
   useMediaQuery,
+  useScrollTrigger,
 } from "@mui/material";
 import DrawerComp from "../Drawer/DrawerComp";
+
+// navbar elevaton when scrolling
+const ElevationScroll = (props) => {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+};
 
 // navbar names
 const pages = [
@@ -43,42 +58,42 @@ const pages = [
   },
 ];
 const Header = () => {
-  const [value, setValue] = useState();
+  // const [value, setValue] = useState();
 
   const theme = useTheme();
-  console.log(theme);
 
   // checking if screen size is md (boolean)
   const isMarch = useMediaQuery(theme.breakpoints.down("md"));
-  console.log(isMarch);
 
   return (
     <>
-      <AppBar position="static">
-        <Toolbar css={bar()}>
-          <Container maxWidth="lg" css={container()}>
-            {/* Logo */}
-            <Typography variant="h6" component="a" href="/" css={logo()}>
-              Jackson Collins-Ikpe
-            </Typography>
-            {/* Menu */}
-            {isMarch ? (
-              <DrawerComp pages={pages} />
-            ) : (
-              <Tabs
-                textColor="secondary"
-                value={value}
-                onChange={(e, value) => setValue(value)}
-                indicatorColor="secondary"
-              >
-                {pages.map((page, index) => (
-                  <Tab label={page.name} href={page.link} key={index} />
-                ))}
-              </Tabs>
-            )}
-          </Container>
-        </Toolbar>
-      </AppBar>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar css={bar()}>
+            <Container maxWidth="lg" css={container()}>
+              {/* Logo */}
+              <Typography variant="h6" component="a" href="/" css={logo()}>
+                Jackson Collins-Ikpe
+              </Typography>
+              {/* Menu */}
+              {isMarch ? (
+                <DrawerComp pages={pages} />
+              ) : (
+                <Tabs
+                  textColor="secondary"
+                  // value={value}
+                  // onChange={(e, value) => setValue(value)}
+                  indicatorColor="secondary"
+                >
+                  {pages.map((page, index) => (
+                    <Tab label={page.name} href={page.link} key={index} />
+                  ))}
+                </Tabs>
+              )}
+            </Container>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
     </>
   );
 };
