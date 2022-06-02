8521ff0b428908c97aa651d5d2eb900ea476cd84
Jackson Collins-Ikpe <jacksoncikpe@gmail.com>
2022-06-02 14:02:31 +0100

    added background animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 import Layout from "../layout/Layout";
+import Hero from "../components/Hero/Hero";
+import About from "../components/About/About";
 
 export default function Home() {
   return (
     <Layout>
-      <div>hello</div>
+      <Hero />
+      <About />
     </Layout>
   );
 }
